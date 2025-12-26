a4a2611a7d5cca884a1d8b9b5143f5bad57001e0
acmdf <233685277+acmdf@users.noreply.github.com>
2025-12-27 06:10:39 +1000

    Add Nix Flake

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "VRCTV Development Environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell { 
+          packages = with pkgs; [ 
+            pkg-config
+            gobject-introspection
+            nodejs
+            pnpm
+            cargo
+            rustc
+          ];  
+          
+          buildInputs = with pkgs; [
+            at-spi2-atk
+            atkmm
+            cairo
+            gdk-pixbuf
+            glib
+            gtk3
+            harfbuzz
+            librsvg
+            libsoup_3
+            pango
+            webkitgtk_4_1
+            openssl
+          ];
+        };
+      }
+    );
+}
